f969a1c48ed08bb414f975cbf43de194f1528611
Md Shadab Alam <shadabalam.1725@gmail.com>
2024-07-23 01:57:33 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+data: any;
+  
+  constructor(private http: HttpClient){}
+
+  baseAPIUrl : string ='https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyBSDdRDS-U9Z4mJIDc8DxRGuXCByXkeO34';
+    
+  input:string='';
+
+  ngOnInit(): void {
+    console.log("loading...");
+    // this.getData().subscribe(
+    //   res=>{
+    //     console.log(res);
+    //     console.log(res.candidates[0].content.parts[0].text);
+        
+    //   }
+    // )
+  }
+  getAnswer(){
+    this.getData().subscribe(
+      res=>{
+        this.data = res.candidates[0].content.parts[0].text
+        console.log(this.data)
+      }
+    )
+  }
+
+  getData():Observable<any>{
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json'
+      });
+  
+      const body = {
+        contents: [
+          {
+            parts: [
+              { text: this.input }
+            ]
+          }
+        ]
+      };
+  
+    return this.http.post<any>(this.baseAPIUrl,body,{headers});
+  }
+
   title = 'AI-Gen-ChatBot';
 }
